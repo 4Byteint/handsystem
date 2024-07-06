d74e1be0795d24a37e80395335bb60566a9e8888
4Byteint <jingwenh476@gmail.com>
2024-07-07 01:39:30 +0800

    class seperation

--- ros2_robot_ws/build/gripper_sub/build/lib/gripper_sub/subscriber_member_function.py
+++ ros2_robot_ws/build/gripper_sub/build/lib/gripper_sub/subscriber_member_function.py
@@ -1,184 +1,10 @@
 # 6/30 整合
-import rclpy
+import time
+import threading
+from gripper_sub.claw import Claw
+from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.node import Node
 from robot_interfaces.msg import GripperCommand, GripperInfo
-from rclpy.executors import MultiThreadedExecutor
-from rclpy.callback_groups import ReentrantCallbackGroup
-import time
-from canlib import canlib, Frame
-import threading
-
-
-class Claw:
-    def __init__(self):
-        self.state = "PowerOn"  # 初始狀態
-        self.ch = canlib.openChannel(
-            channel=0, flags=canlib.Open.EXCLUSIVE, bitrate=canlib.canBITRATE_1M
-        )
-        self.ch.setBusOutputControl(canlib.Driver.NORMAL)
-        self.ch.busOn()
-        self.power_flag = False
-        self.ros_flag = False
-        self.sendSTM_flag = False
-        self.sendUNO_flag = False
-        self.sendingTimeStart_STM = 0
-        self.sendingTimeStart_UNO = 0
-
-    def power_on(self):
-        # 上電後的初始化操作，例如檢查電源、啟動系統、檢查各個device是否開啟
-        # while True:
-        #   # 在背景執行 process
-        #   process = subprocess.Popen(["python3", "digit_check.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        #   time.sleep(3)
-        #   if process.poll() is None:
-        #     print("camera is running.")
-        #     self.state = "CheckConnection"
-        #     break
-        #   else:
-        #     print("digit not found")
-
-        self.state = "CheckConnection"
-
-    def check_connection(self):
-        ask_Boot = Frame(id_=1, data=[0, 1, 1, 1, 0, 0, 0, 0], dlc=8)
-        while True:
-            user_input = input("please enter a to start: ")
-            if user_input == "a":
-                self.ch.write(ask_Boot)
-                time.sleep(0.1)
-                break
-        count = 0
-        U_flag = False
-        STM_flag = False
-        start_time = time.time()
-        while count < 2:
-            try:
-                msg = self.ch.read(timeout=5000)  # timeout 機制
-                print(msg)
-                if (msg.data[2] == 2) and msg.data[3] == 1:  # STM
-                    count += 1
-                    STM_flag = True
-                elif (msg.data[2] == 3) and msg.data[3] == 1:  # UNO
-                    count += 1
-                    U_flag = True
-                if STM_flag and U_flag:
-                    break
-            except canlib.CanNoMsg:
-                if time.time() - start_time > 5:
-                    count = 0
-                    if not U_flag:
-                        U_flag = False
-                        print("沒收到 UNO 開機，重新發送")
-                    if not STM_flag:
-                        STM_flag = False
-                        print("沒收到 STM 開機，重新發送")
-                    self.ch.write(ask_Boot)
-        if STM_flag and U_flag:
-            return True
-
-    def initialization(self):
-        # 1. 發送初始化指令
-        U_init = Frame(id_=3, data=[0, 1, 1, 2, 0, 0, 0, 0], dlc=8)
-        STM_init = Frame(
-            id_=2, data=[0, 1, 1, 3, 10, 0, 0, 0], dlc=8
-        )  # 初始值10 (for STM)
-        self.ch.write(U_init)
-        self.ch.write(STM_init)
-        # 2. 等待初始化完成
-        count = 0
-        U_flag = False
-        STM_flag = False
-        start_time = time.time()
-        while count < 2:
-            try:
-                msg = self.ch.read(timeout=5000)  # timeout 機制
-                print(msg)
-                if (msg.data[2] == 2) and msg.data[3] == 2:  # STM
-                    count += 1
-                    STM_flag = True
-                elif (msg.data[2] == 3) and msg.data[3] == 2:  # UNO
-                    count += 1
-                    U_flag = True
-                if STM_flag and U_flag:
-                    break
-            except canlib.CanNoMsg:
-                if time.time() - start_time > 5:
-                    count = 0
-                    if not U_flag:
-                        U_flag = False
-                        print("沒收到 UNO 初始化，重新發送")
-                        self.ch.write(U_init)
-                    if not STM_flag:
-                        STM_flag = False
-                        print("沒收到 STM 初始化，重新發送")
-                        self.ch.write(STM_init)
-        if STM_flag and U_flag:
-            return True
-
-    def grabbing(self):
-        
-        if  self.sendUNO_flag:
-            sensor_request = Frame(id_=3, data=[0, 1, 1, 6, 0, 0, 0, 0], dlc=8)
-            self.ch.write(sensor_request)
-            self.sendUNO_flag=False
-        
-        if  self.sendSTM_flag:
-            print("grabbing... ")
-            grab_action = Frame(id_=2, data=[0, 1, 1, 4, 200, 0, 0, 0], dlc=8)
-            self.ch.write(grab_action)
-            self.sendSTM_flag=False
-            self.sendingTimeStart_STM = time.time()
-        else:
-            try:
-                msg = self.ch.read(timeout=5000)  # timeout 機制
-                print(msg)
-                if (msg.data[2] == 2) and msg.data[3] == 4:  # STM
-                    print("STM開始夾")
-                    return True
-                # elif :
-                #     print("STM尚未開夾,重新發送")
-                #     self.sendSTM_flag = True
-                #     return False
-            except canlib.CanNoMsg:
-                if time.time() - self.sendingTimeStart_STM > 5:
-                    self.sendSTM_flag = True
-                    print("未收到STM是否開夾,重新發送")
-                    return False
-
-    def Release(self):
-        
-        print("sendSTM_flag")
-        print(self.sendSTM_flag)
-        if self.sendSTM_flag:
-            start_release = Frame(id_=2, data=[0, 1, 1, 5, 10, 0, 0, 0], dlc=8)
-            self.ch.write(start_release)
-            self.sendSTM_flag=False
-            # self.sendingTimeStart_STM = time.time()
-            print(time.time()-self.sendingTimeStart_STM)
-        else:
-            try:
-                msg = self.ch.read(timeout=5000)  # timeout 機制
-                print(msg)
-                if (msg.data[2] == 2) and msg.data[3] == 5:  # STM
-                    print("c8 c8 c8 88c8 8c88 c88c 8c88 8c")
-                    print("STM開始放開")
-                    return True
-                # else:
-                #     print("STM尚未放開,重新發送")
-                #     self.sendSTM_flag = True
-                #     return False
-            except canlib.CanNoMsg:
-                if time.time()-self.sendingTimeStart_STM>5:
-                    self.sendSTM_flag = True
-                    print("未收到STM是否放開,重新發送")
-                    return False
-                  
-        
-
-    def shutdown(self):
-        # 關閉機器
-        self.ch.busOff()
-        self.ch.close()
 
 
 #########################################################################
@@ -186,7 +12,6 @@ class Claw:
 #########################################################################
 #########################################################################
 #########################################################################
-
 
 
 class pubsub(Node, Claw):
@@ -264,7 +89,7 @@ class pubsub(Node, Claw):
             # msg.num = 1
             # msg.resp = 1
             # self.publisher_resp.publish(msg)
-            
+
             self.claw.sendSTM_flag = True
             self.claw.sendUNO_flag = True
             print(self.claw.sendSTM_flag)
@@ -305,7 +130,7 @@ class pubsub(Node, Claw):
             # print("開始夾取流程")
             if self.claw.grabbing():
                 msg = GripperInfo()
-                msg.result=1
+                msg.result = 1
                 self.publisher_info.publish(msg)
                 print("回傳已夾取")
                 self.claw.state = "wait_for_command"
@@ -314,7 +139,7 @@ class pubsub(Node, Claw):
             # print("開始放下流程")
             if self.claw.Release():
                 msg = GripperInfo()
-                msg.result=3
+                msg.result = 3
                 self.publisher_info.publish(msg)
                 print("回傳已放開")
                 self.claw.state = "wait_for_command"
@@ -328,27 +153,3 @@ class pubsub(Node, Claw):
         #     if self.claw.Hold():
         #         print("開始夾取!")
         #         pass
-
-
-def main(args=None):
-    rclpy.init(args=args)
-
-    pubsub_instance = pubsub()
-    # Claw_instance = Claw()
-    # 使用 MultiThreadedExecutor 運行節點
-    executor = MultiThreadedExecutor()
-    executor.add_node(pubsub_instance)
-    try:
-        executor.spin()
-    finally:
-        executor.shutdown()
-        pubsub_instance.destroy_node()
-        pubsub_instance.claw.shutdown()
-        rclpy.shutdown()
-
-
-# def testForGit():
-#     print("放下完成")
-
-if __name__ == "__main__":
-    main()
---
+++ ros2_robot_ws/src/gripper_sub/gripper_sub/claw.py
@@ -0,0 +1,172 @@
+from canlib import canlib, Frame
+import time
+
+
+class Claw:
+    def __init__(self):
+        self.state = "PowerOn"  # 初始狀態
+        self.ch = canlib.openChannel(
+            channel=0, flags=canlib.Open.EXCLUSIVE, bitrate=canlib.canBITRATE_1M
+        )
+        self.ch.setBusOutputControl(canlib.Driver.NORMAL)
+        self.ch.busOn()
+        self.power_flag = False
+        self.ros_flag = False
+        self.sendSTM_flag = False
+        self.sendUNO_flag = False
+        self.sendingTimeStart_STM = 0
+        self.sendingTimeStart_UNO = 0
+
+    def power_on(self):
+        # 上電後的初始化操作，例如檢查電源、啟動系統、檢查各個device是否開啟
+        # while True:
+        #   # 在背景執行 process
+        #   process = subprocess.Popen(["python3", "digit_check.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        #   time.sleep(3)
+        #   if process.poll() is None:
+        #     print("camera is running.")
+        #     self.state = "CheckConnection"
+        #     break
+        #   else:
+        #     print("digit not found")
+
+        self.state = "CheckConnection"
+
+    def check_connection(self):
+        ask_Boot = Frame(id_=1, data=[0, 1, 1, 1, 0, 0, 0, 0], dlc=8)
+        while True:
+            user_input = input("please enter a to start: ")
+            if user_input == "a":
+                self.ch.write(ask_Boot)
+                time.sleep(0.1)
+                break
+        count = 0
+        U_flag = False
+        STM_flag = False
+        start_time = time.time()
+        while count < 2:
+            try:
+                msg = self.ch.read(timeout=5000)  # timeout 機制
+                print(msg)
+                if (msg.data[2] == 2) and msg.data[3] == 1:  # STM
+                    count += 1
+                    STM_flag = True
+                elif (msg.data[2] == 3) and msg.data[3] == 1:  # UNO
+                    count += 1
+                    U_flag = True
+                if STM_flag and U_flag:
+                    break
+            except canlib.CanNoMsg:
+                if time.time() - start_time > 5:
+                    count = 0
+                    if not U_flag:
+                        U_flag = False
+                        print("沒收到 UNO 開機，重新發送")
+                    if not STM_flag:
+                        STM_flag = False
+                        print("沒收到 STM 開機，重新發送")
+                    self.ch.write(ask_Boot)
+        if STM_flag and U_flag:
+            return True
+
+    def initialization(self):
+        # 1. 發送初始化指令
+        U_init = Frame(id_=3, data=[0, 1, 1, 2, 0, 0, 0, 0], dlc=8)
+        STM_init = Frame(
+            id_=2, data=[0, 1, 1, 3, 10, 0, 0, 0], dlc=8
+        )  # 初始值10 (for STM)
+        self.ch.write(U_init)
+        self.ch.write(STM_init)
+        # 2. 等待初始化完成
+        count = 0
+        U_flag = False
+        STM_flag = False
+        start_time = time.time()
+        while count < 2:
+            try:
+                msg = self.ch.read(timeout=5000)  # timeout 機制
+                print(msg)
+                if (msg.data[2] == 2) and msg.data[3] == 2:  # STM
+                    count += 1
+                    STM_flag = True
+                elif (msg.data[2] == 3) and msg.data[3] == 2:  # UNO
+                    count += 1
+                    U_flag = True
+                if STM_flag and U_flag:
+                    break
+            except canlib.CanNoMsg:
+                if time.time() - start_time > 5:
+                    count = 0
+                    if not U_flag:
+                        U_flag = False
+                        print("沒收到 UNO 初始化，重新發送")
+                        self.ch.write(U_init)
+                    if not STM_flag:
+                        STM_flag = False
+                        print("沒收到 STM 初始化，重新發送")
+                        self.ch.write(STM_init)
+        if STM_flag and U_flag:
+            return True
+
+    def grabbing(self):
+
+        if self.sendUNO_flag:
+            sensor_request = Frame(id_=3, data=[0, 1, 1, 6, 0, 0, 0, 0], dlc=8)
+            self.ch.write(sensor_request)
+            self.sendUNO_flag = False
+
+        if self.sendSTM_flag:
+            print("grabbing... ")
+            grab_action = Frame(id_=2, data=[0, 1, 1, 4, 200, 0, 0, 0], dlc=8)
+            self.ch.write(grab_action)
+            self.sendSTM_flag = False
+            self.sendingTimeStart_STM = time.time()
+        else:
+            try:
+                msg = self.ch.read(timeout=5000)  # timeout 機制
+                print(msg)
+                if (msg.data[2] == 2) and msg.data[3] == 4:  # STM
+                    print("STM開始夾")
+                    return True
+                # elif :
+                #     print("STM尚未開夾,重新發送")
+                #     self.sendSTM_flag = True
+                #     return False
+            except canlib.CanNoMsg:
+                if time.time() - self.sendingTimeStart_STM > 5:
+                    self.sendSTM_flag = True
+                    print("未收到STM是否開夾,重新發送")
+                    return False
+
+    def Release(self):
+
+        print("sendSTM_flag")
+        print(self.sendSTM_flag)
+        if self.sendSTM_flag:
+            start_release = Frame(id_=2, data=[0, 1, 1, 5, 10, 0, 0, 0], dlc=8)
+            self.ch.write(start_release)
+            self.sendSTM_flag = False
+            # self.sendingTimeStart_STM = time.time()
+            print(time.time() - self.sendingTimeStart_STM)
+        else:
+            try:
+                msg = self.ch.read(timeout=5000)  # timeout 機制
+                print(msg)
+                if (msg.data[2] == 2) and msg.data[3] == 5:  # STM
+                    print("c8 c8 c8 88c8 8c88 c88c 8c88 8c")
+                    print("STM開始放開")
+                    return True
+                # else:
+                #     print("STM尚未放開,重新發送")
+                #     self.sendSTM_flag = True
+                #     return False
+            except canlib.CanNoMsg:
+                if time.time() - self.sendingTimeStart_STM > 5:
+                    self.sendSTM_flag = True
+                    print("未收到STM是否放開,重新發送")
+                    return False
+
+    def shutdown(self):
+        # 關閉機器
+        self.ch.busOff()
+        self.ch.close()
